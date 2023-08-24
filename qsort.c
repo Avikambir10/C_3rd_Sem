89a6048e78da2e23fb72708704d216cffb7e3892
Avikambir10 <123386663+Avikambir10@users.noreply.github.com>
2023-08-24 10:04:28 +0530

    Important

---
+++ qsort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+int cmp(const void * a ,const void * b) {
+    return *(int *)a - *(int *)b;  
+}
+int main() {
+    int arr[] = {5,4,2,1,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    qsort(arr,n,sizeof(arr[0]), cmp);
+    for (int  i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
